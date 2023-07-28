bcf8c739c9b597232bf187d2a39ab575cf36b834
Nassor Nassor <administrator@Nassors-MacBook-Pro.local>
2023-07-28 11:01:42 +0300

    feat: adding new login page design

---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Accident Notification</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/custom.css">
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="container pt-5">
+            <div class="row justify-content-center  pt-5">
+                <div class="col-md-5 pt-5">
+                    <div class="login-form">
+                        <div class="text-center">
+                            <h2 class="my-3">Accident Push Notification</h2>
+                            <h3>Login</h3>
+                        </div>
+                        <form action="#">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" id="username" class="form-control" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" class="form-control" placeholder="Password">
+                            </div>
+                            <hr>
+                            <div class="d-flex justify-content-between">
+                                <button class="btn btn-primary">Register</button>
+                                <button class="btn btn-primary">Login</button>
+                            </div>
+                            <h6 class="text-center mt-3">Accident Push Notification&copy;2023</h6>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
